8f05c4b831adb8e82f9c8015a16f6d1ad58640a1
tj11 <tedwardj11@gmail.com>
2018-03-08 13:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function takeANumber(katzDeliLine,newName) {
   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing()
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    return katzDeliLine.shift()
+  }
+}
