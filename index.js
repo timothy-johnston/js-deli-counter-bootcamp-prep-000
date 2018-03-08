625f3f4adde97e94387d299afd10322b4391d855
tj11 <tedwardj11@gmail.com>
2018-03-08 13:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
+  if (katzDeliLine.length===0) {
+    return "The line is currently empty."
+  }
+  
+  
   for (i=0;i<katzDeliLine.length;i++) {
     if (katzDeliLIne.length===0) {
       return "The line is currently empty."
