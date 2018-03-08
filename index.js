774cfcc23f6c124157b987967784e1f0bd313d10
tj11 <tedwardj11@gmail.com>
2018-03-08 13:46:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,16 +17,29 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length===0) {
     return "The line is currently empty."
   }
+ 
+
   
-    for (i=0;i<katzDeliLine.length;i++) {
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    for (var i=0;i<katzDeliLine.length;i++) {
       if (i===0) {
         sentence = `The line is currently: 1. ${katzDeliLine[i]}`
       } else {
         sentence = sentence + `, ${i+1} ${katzDeliLine[i]}`
       }
     }
-
 }
+
      
   
  
