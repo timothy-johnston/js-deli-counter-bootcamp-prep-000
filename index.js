e6ae4b38ecdf296b6c0086c1bb3a34657ab0f509
tj11 <tedwardj11@gmail.com>
2018-03-08 13:24:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //var katzDeli=[];
-
+var katzDeliline=[];
 function takeANumber(katzDeliLine,newName) {
-  
+  katzDeliLine.push(newName)
+  return `Welcome ${newName}. You are number ${katzDeliLine.length} in line.`
 }
